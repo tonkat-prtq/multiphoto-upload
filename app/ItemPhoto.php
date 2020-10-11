29bd41a6356c7e4b602fabc322f58b68fb6357a9
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-11 19:33:42 +0900

    Add: ItemPhotoモデルにfillableと関連付け追記

--- app/ItemPhoto.php
+++ app/ItemPhoto.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ItemPhoto extends Model
 {
-    //
+    protected $fillable = [
+        'item_id',
+        'path'
+    ];
+
+    public function item()
+    {
+        return $this->belongsTo('App\Item');
+    }
 }
