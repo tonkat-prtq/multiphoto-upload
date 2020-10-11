df26a4008c2a1049cd1276bf64fe469c43835538
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-11 19:36:21 +0900

    Add: ItemControllerにindexとcreateアクション追加

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    public function index()
+    {
+        $items = Item::all();
+        return view('item.index', compact('items'));
+    }
+
+    public function create(Request $request)
+    {
+        // POST
+        if ($request->isMethod('POST')) {
+            dd($request->all());
+        }
+
+        // GET
+        return view('item.create');
+    }
 }
