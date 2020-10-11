9efc9604c44840120be95dfbdff28ab6066e162e
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-11 19:12:47 +0900

    first commit

---
+++ database/migrations/2020_10_11_101029_create_item_photos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemPhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_photos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('item_id')->unsigned();
+            $table->foreign('item_id')->references('id')->on('items');
+            $table->string('path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_photos');
+    }
+}
