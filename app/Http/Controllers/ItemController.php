d6a42dde96cf37563d606e023f5162ff9a745dd0
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-11 20:08:44 +0900

    Add: ddをif文の中に入れるも反応しない→メソッドがPOSTになっていない？

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Item;
 use Illuminate\Http\Request;
+
 
 class ItemController extends Controller
 {
@@ -15,9 +17,30 @@ class ItemController extends Controller
     public function create(Request $request)
     {
         // POST
+        
         if ($request->isMethod('POST')) {
+
+            // 商品情報の保存
+            $item = Item::create([
+                'jan' => $request->jan,
+                'name' => $request->name
+                ]);
+            
+
+            // 商品画像の保存
+            foreach ($request->file('files') as $index=> $e) {
+                $ext = $e['photo']->guessExtension();
+                $filename = "{$request->jan}_{$index}.{$ext}";
+                $path = $e['photo']->storeAs('photos', $filename);
+                // photosメソッドにより、商品に紐付けられた画像を保存する
+                $item->photos()->create(['path'=> $path]);
+            }
+
             dd($request->all());
+
+            return redirect('/items')->with(['success' => '保存しました']);
         }
+
 
         // GET
         return view('item.create');
