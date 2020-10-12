8c425ec041cb83819c2e655a9e8025fa6e94e384
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-12 20:55:57 +0900

    Add: コメント追加

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -29,13 +29,26 @@ class ItemController extends Controller
                 ]);
             
 
-            // 商品画像の保存
+            /*
+                商品画像の保存
+                $index には それが何枚目の画像かという情報が入ってる？
+                $eには画像の情報
+                $extにはファイルの形式
+                $filenameにはファイルの名前
+            */
+
+            // アップロードされたファイルの数だけ繰り返す
             foreach ($request->file('files') as $index=> $e) {
+                // guessExtensionでアップロードしたファイルの拡張子を取得
                 $ext = $e['photo']->guessExtension();
+                // 変数$filenameに、"jan番号"_"何枚目の画像か"."ファイルの形式"とリネーム用の文字列を準備する
                 $filename = "{$request->jan}_{$index}.{$ext}";
+                // storeAsで保存するファイル名を指定して、'photos'ディスクに保存、そのパスを変数$pathに入れている\
                 $path = $e['photo']->storeAs('photos', $filename);
                 // photosメソッドにより、商品に紐付けられた画像を保存する
                 $item->photos()->create(['path'=> $path]);
+                
+                // dd($index, $e, $ext, $filename, $path);
             }
 
             return redirect('/items')->with(['success' => '保存しました']);
